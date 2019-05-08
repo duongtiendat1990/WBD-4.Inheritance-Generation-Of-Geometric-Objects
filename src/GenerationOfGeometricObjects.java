2b3f35a3c44083f4ee6d9c1bb3327be5a3f61f58
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-05-08 08:26:19 +0700

    Geometric Generation

---
+++ src/GenerationOfGeometricObjects.java
@@ -0,0 +1,13 @@
+public class GenerationOfGeometricObjects {
+  public static void main(String[] args) {
+    Square square = new Square();
+    System.out.println(square);
+
+    square = new Square(2.3);
+    System.out.println(square);
+
+    square = new Square( "yellow", true,5.8);
+    System.out.println(square);
+
+  }
+}
